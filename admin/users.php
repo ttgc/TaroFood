0ae2395b7be2e31d23a404891bb2874fad63eaee
Thomas PIOT <piott@3il.fr>
2019-01-16 10:42:18 +0100

    creating branch for user management panel

---
+++ admin/users.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <?php require 'includes/head.php'; ?>
+  <body>
+    <?php require 'includes/header.php'; ?>
+
+    <?php require 'includes/footer.php'; ?>
+  </body>
+</html>
