8afde713cf33d0f0203a8cb178798d28361d9746
Thomas PIOT <piott@3il.fr>
2019-01-16 10:39:04 +0100

    adding fontawesome to head on admin

--- admin/includes/head.php
+++ admin/includes/head.php
@@ -1,5 +1,6 @@
 <head>
   <title>Taro Food</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
   <link rel="stylesheet" href="../css/admin.css">
 </head>
